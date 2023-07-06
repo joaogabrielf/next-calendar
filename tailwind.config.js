7eab21009f92fae6bdae1a5f93b59987625192fc
Joao Franco <joaogabriel@pm.me>
2023-07-06 07:31:02 -0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+import {
+  colors,
+  borderRadius,
+  fontFamily,
+  fontSize,
+  fontWeight,
+  lineHeight,
+} from '@joaogabrielf/tokens'
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@joaogabrielf/react/**/*.{js,ts,jsx,tsx,mdx, d.ts}',
   ],
   theme: {
     extend: {
@@ -12,6 +23,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors,
+      borderRadius,
+      fontFamily,
+      fontSize,
+      fontWeight,
+      lineHeight,
     },
   },
   plugins: [],
